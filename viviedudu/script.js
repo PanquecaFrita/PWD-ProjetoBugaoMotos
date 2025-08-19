75fc81c6ba0bb195a27548b59d62a4c4ca4c4d63
PanquecaFrita <vivianeguimaraes080915@gmail.com>
2025-08-19 16:44:35 -0400

    vivi e dudu

---
+++ viviedudu/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".btn-success").addEventListener("click", () => {
+    alert("Dados salvos!");
+  });
+  
+  document.querySelector(".btn-danger").addEventListener("click", () => {
+    confirm("Deseja realmente deletar?");
+  });
+  
+  document.querySelector(".btn-dark").addEventListener("click", () => {
+    alert("Listando registros...");
+  });
+  
